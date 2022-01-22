29d19e069842ae515cfa27b39b6537a1761066ae
MinhThao <minhthaouit@gmail.com>
2022-01-22 22:06:52 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nguyễn Minh Thao
         </p>
         <a
           className="App-link"
